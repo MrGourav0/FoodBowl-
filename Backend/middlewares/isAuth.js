5416e850335d628edaafb3bc60c75817cf8bed01
Gourav verma <gouravverma8319@gmail.com>
2025-12-09 10:27:44 +0530

    fixing the same issue 404 in axios

--- Backend/middlewares/isAuth.js
+++ Backend/middlewares/isAuth.js
@@ -3,16 +3,13 @@ const isAuth=async (req,res,next) => {
     try {
         const token=req.cookies.token
         if(!token){
-            return res.status(400).json({message:"token not found"})
+            return res.status(401).json({message:"Unauthorized: No token provided"})
         }
         const decodeToken=jwt.verify(token,process.env.JWT_SECRET)
-        if(!decodeToken){
- return res.status(400).json({message:"token not verify"})
-        }
         req.userId=decodeToken.userId
         next()
     } catch (error) {
-         return res.status(500).json({message:"isAuth error"})
+         return res.status(401).json({message:"Unauthorized: Invalid token"})
     }
 }
 
