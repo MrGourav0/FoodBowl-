0a91713d2b975db909dace62067bd09f7b8e2c2b
Gourav verma <gouravverma8319@gmail.com>
2025-12-09 10:24:20 +0530

    network axios erro fix kara

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getCurrentUser } from '../controllers/user.controllers.js';
+import { getCurrentUser, updateUserLocation } from '../controllers/user.controllers.js';
 import isAuth from '../middlewares/isAuth.js';
 
 const router = express.Router();
 
 router.get('/current', isAuth, getCurrentUser);
+router.post('/update-location', isAuth, updateUserLocation);
 
 export default router;
