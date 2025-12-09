a2315ff80c219cf166c15d08a699f9eaf7b72023
Gourav verma <gouravverma8319@gmail.com>
2025-12-09 10:16:38 +0530

    fix the axios issue

--- Backend/controllers/auth.controllers.js
+++ Backend/controllers/auth.controllers.js
@@ -27,8 +27,8 @@ export const signUp=async (req,res) => {
 
         const token=await genToken(user._id)
         res.cookie("token",token,{
-            secure:false,
-            sameSite:"strict",
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             maxAge:7*24*60*60*1000,
             httpOnly:true
         })
@@ -57,8 +57,8 @@ export const signIn=async (req,res) => {
 
         const token=await genToken(user._id)
         res.cookie("token",token,{
-            secure:false,
-            sameSite:"strict",
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             maxAge:7*24*60*60*1000,
             httpOnly:true
         })
@@ -147,8 +147,8 @@ export const googleAuth=async (req,res) => {
 
         const token=await genToken(user._id);
         res.cookie("token",token,{
-            secure:false,
-            sameSite:"strict",
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             maxAge:7*24*60*60*1000,
             httpOnly:true
         });
